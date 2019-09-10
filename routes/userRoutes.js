ae28ee849e9400ffcdf447239a7fb99b554f73a9
Martin Melo <nitram-210397@hotmail.com>
2019-09-10 08:17:20 -0500

    Change password user route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/UserController')
+const bcrypt = require('bcryptjs')
 
 router.get('/', (req, res) => {
     if (!req.query.filter) {
@@ -53,6 +54,33 @@ router.delete('/:id', (req, res) => {
         .catch(error => {
             return res.status(400).json(error)
         })
+})
+
+router.post('/changePassword/:id', (req, res) => {
+    const { newPassword } = req.body
+    const { id } = req.params
+    if (newPassword) {
+        bcrypt.genSalt(10, (err, salt) => {
+            if (err) throw err
+            bcrypt.hash(newPassword, salt, (err, hashed) => {
+                if (err) throw err
+                const data = {
+                    password: hashed,
+                    logged: true
+                }
+                userController.update(id, data)
+                    .then(result => {
+                        if (result.hasError) {
+                            return res.status(400).json(result.error)
+                        }
+                        return res.json(result)
+                    })
+                    .catch(error => {
+                        return res.status(400).json(error)
+                    })
+            })
+        })
+    }
 })
 
 module.exports = router
